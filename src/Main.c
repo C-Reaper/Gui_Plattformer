f0fa652dbf53e01a7e099d415b9602a06e83c667
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:35:52 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,4 @@
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/Yaml.h"
 #include "/home/codeleaded/System/Static/Library/Geometry3D.h"
 
